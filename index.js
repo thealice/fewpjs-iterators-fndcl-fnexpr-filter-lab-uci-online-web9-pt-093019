3700a65d880d3f90b555f2046df55470018f6edd
Alice Freda <alicefreda@gmail.com>
2020-05-17 19:53:49 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+function findMatching(data, query) {
+    const lowercaseAsk = query.toLowerCase();
+    return data.filter(x => {
+        return x.toLowerCase() === lowercaseAsk;
+    });
+}
+
+function fuzzyMatch(data, query) {
+    const regEx = RegExp(`^${query}`, 'i');
+    return data.filter(x => {
+        if (regEx.test(x)) {
+            return x;
+        }
+    });
+}
+
+function matchName(data, query) {
+    const lowercaseAsk = query.toLowerCase();
+    return data.filter(x => {
+        if (x.name.toLowerCase() === lowercaseAsk)
+            return `{
+                name: ${x.name},
+                hometown: ${x.hometown}
+            }`
+    })
+}
